3972497ea85a98f7ef5c27fda4d650e175fdee24
Sajad <sajadsoorni@gmail.com>
2025-03-19 17:54:20 +0000

    feat: practice object destructuring in JavaScript
    - Destructure properties from objects using curly braces.
    - Rename variables while destructuring.
    - Assign default values to destructured properties.
    - Mutate existing variables using destructuring.
    - Extract values from nested objects.
    - Implement destructuring in function parameters for improved readability.

--- destructuringArrays.js
+++ destructuringArrays.js
@@ -6,12 +6,70 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Vivian Road',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Vivian Road',
+  time: '21:15',
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*
+/////////////////////////////////////////////////////
+// Destructurinf Arrays
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -46,3 +104,4 @@ console.log(i, j, k, l);
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+*/

54aa499c159687a45abfa2b8ede6bab24e5a0300
Sajad <sajadsoorni@gmail.com>
2025-03-20 18:52:27 +0000

    feat: implement rest pattern and parameters
    - Introduced rest pattern in array and object destructuring
    - Used rest parameters in functions to handle multiple arguments
    - Demonstrated the difference between spread and rest syntax
    - Implemented examples for restaurant menu operations

--- destructuringArrays.js
+++ destructuringArrays.js
@@ -34,8 +34,47 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
 
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizaa, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizaa, risotto, otherFood);
+
+// Objects
+const { sat, ...weekDays } = restaurant.openingHours;
+console.log(weekDays);
+
+// Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i in numbers) {
+    sum += numbers[i];
+    console.log(sum);
+  }
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushroom');
+
+/*
+///////////////////////////////////////////////
+// The Spread Operator ( ... )
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -85,6 +124,7 @@ const resturantCopy = { ...restaurant };
 resturantCopy.name = 'Risturante Roma';
 console.log(resturantCopy.name);
 console.log(restaurant.name);
+*/
 
 /*
 ///////////////////////////////////////////////

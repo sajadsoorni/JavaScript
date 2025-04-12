0a092e0e71b76aa7c7d8cb5c4a7906847163dc1c
Sajad <sajadsoorni@gmail.com>
2025-04-12 09:30:15 +0100

    feat(strings): add split, join, pad, mask, and repeat examples
    - Demonstrated splitting strings using split() and destructuring to assign first and last names.
    - Created a formatted name using join() to combine "Mr.", first name, and uppercase last name.
    - Implemented capitalizeName() function to capitalize the first letter of each word.
    - Showcased padStart() and padEnd() to pad strings for messages.
    - Added maskCreditCard() to mask credit card numbers except the last four digits.
    - Utilized repeat() for repeating messages and visualizing planes in line with emojis.

--- destructuringArrays.js
+++ destructuringArrays.js
@@ -48,6 +48,59 @@ const restaurant = {
   },
 };
 
+console.log('a+very+nice+string'.split('+'));
+console.log('Sajad Soornisofla'.split(' '));
+
+const [firstName, lastName] = 'Sajad Soornisofla'.split(' ');
+
+const newName = ['M.r', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const nameUpper = [];
+
+  for (const n of names) {
+    // nameUpper.push(n[0].toUpperCase() + n.slice(1));
+    nameUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+
+  console.log(nameUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('sajad soornisofla');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Sajad'.padStart(25, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad weather all Departures delayed... \n';
+
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+/*
+//////////////////////////////////////////
+// Working with String - Part 2
 const airLine = 'TAP Air Portuqal';
 
 console.log(airLine.toLowerCase());
@@ -108,6 +161,7 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+*/
 
 /*
 //////////////////////////////////////////////
